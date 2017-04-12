3f4e0260e2e9de6951c1f4be4f56fb4e2eff8f92
Oleksandr Khodakovskyi <Oleksandr_Khodakovskyi@epam.com>
2017-04-12 20:38:35 +0300

    some changes

--- app/controllers/MyApp.java
+++ app/controllers/MyApp.java
@@ -8,4 +8,17 @@ public class MyApp extends Controller{
     public Result index(){
         return ok("It works1!\n\n\n FINE!!");
     }
+
+
+    public Result reqWithParams3(String name){
+        return ok("Welcome to Play, " + name);
+    }
+
+    public Result reqWithParams(String name){
+        return seeOther("/");
+    }
+
+    public Result reqWithParams2(String name){
+        return notFound("<h1>Page not found, " + name + "</h1>").as("text/html");
+    }
 }
